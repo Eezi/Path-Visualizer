39482ac98e8fa49493903a24521ebc8c03ace6ca
Eezi <tomi.a.salo@outlook.com>
2020-09-05 12:42:56 +0300

    Navbar done

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Nav />
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Nav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #fff;
+  height: 100vh;
+
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,73 @@
+<template>
+  <header>
+      <p>Path Visualizer</p>
+
+      <nav>
+          <ul>
+              <li class="visualize">Visualize!</li>
+              <li>Dijkstra's Algorithm</li>
+              <li>Clear Board</li>
+              <li>Speed</li>
+          </ul>
+      </nav>    
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+header {
+     background-color: #2c3e50;
+     display: grid;
+     grid-template-columns: repeat(2, auto);
+     top: 0;
+     left: 0;
+     position: absolute;
+     width: 100% !important;
+     font-size: 1rem;
+ }
+
+ p {
+     padding: 1em 2em;
+     display: block;
+     text-transform: uppercase;
+     font-weight: bold;
+     color: #fff;
+     font-size: 1em;
+ }
+
+ nav {
+     justify-self: right;
+ }
+ ul {
+     list-style: none;
+ }
+li {
+    border-radius: 2%;
+    display: inline;
+    padding: 1em 2em;
+    display: inline-block;
+}
+.visualize {
+    border-radius: 5%;
+    padding: 1em;
+    background-color: #42b983;
+    color: #fff;
+    margin-right: 205px;
+}
+.visualize:hover{
+    background-color: #338560;
+    color: #fff;
+    cursor: pointer;
+}
+li:hover {
+    color: #42b983;
+    cursor: pointer;
+}
+
+</style>>
+ 
